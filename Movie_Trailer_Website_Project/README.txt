1009bef08a6c24ec496d86c2412bb8137d8ea7d0
Kamyab Nazari <kamyabnz@gmail.com>
2020-04-05 16:58:17 +0200

    The Updated Movie Trailer Project with Readme File

---
+++ Movie_Trailer_Website_Project/README.txt
@@ -0,0 +1,34 @@
+Welcome to the Movie Trailer Project
+
+* Code has been checked under the PEP8 Guidelines
+
+File Structure:
+    - media.py
+    - entertainment_center.py
+    - fresh_tomatoes.py
+    - README.txt
+
+Introduction on how to run the Movie Trailer Project:
+    1. Check if python is installed and you have it in your
+       PATH Variable so you can call it with 'python -version'
+       through the CMD or your preferd Shell.
+    2. Open your Python IDLE.
+    3. Through that got to File and then Open up the 
+       'entertainment_center.py' python file.
+    4. In the new opened window of 'entertainment_center.py'
+       choose Run and Run Module.
+    5. You should get the "THE fresh_tomatoes.html HAS BEEN CREATED"
+       message inside of the python IDLE console.
+    6. Now you have created the static website and it should be
+       inside of the same folder as of the project files
+    The new Files Structure:
+        - __pycache__ (Folder)
+            - fresh_tomatoes.cpython-xx.pyc
+            - media.cpython-xx.pyc
+        - fresh_tomatoes.html
+        - media.py
+        - entertainment_center.py
+        - fresh_tomatoes.py
+        - README.txt
+    7. Now you should double click on the fresh_tomatoes.html and open it
+       inside one of your browser's.
--- Movie_Trailer_Website_Project/entertainment_center.py
+++ Movie_Trailer_Website_Project/entertainment_center.py
@@ -1,37 +1,41 @@
 import media 
 import fresh_tomatoes
 
+
+# The instances of Movie class with each specific attributes
+# as parameters, we have the following Instances.
 toy_story = media.Movie("Toy Story", 
-                        "a story of a boy that plays with his toys",
+                        "A story of a boy that plays with his toys",
                         "https://gfx.videobuster.de/archive/v/c-MvmFJ6r-BNo00ZPIHKOTwcz0lMkawrSUyRjAyJTJGaW1hmSUyRmpwZWclMkZiZjVhNGJjv7bDZN7psTFjYzBj7jBm9jUuanBnJnI9d-84/toy-story.jpg",
                         "https://www.youtube.com/watch?v=rNk1Wi8SvNc")
 
 avatar = media.Movie("Avatar", 
-                    "a marine on a different planet",
+                    "A marine on a different planet",
                     "https://i.pinimg.com/originals/24/e1/8a/24e18a4a2f82ba68b6e835c7178285a5.jpg",
                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 shutter_island = media.Movie("Shutter Island", 
-                    "a police officer is investingating a murder on an isnland",
+                    "A police officer is investingating a murder on an isnland",
                     "https://s1.thcdn.com/productimg/0/600/600/63/10080563-1280330043-579000.jpg",
                     "https://www.youtube.com/watch?v=5iaYLCiq5RM")
 
 wind_river = media.Movie("Wind River", 
-                    "a murder has accured in Canada",
+                    "A murder has accured in Canada",
                     "https://www.wildbunch-germany.de/api/wildbunch/poster_image%2Fwindriver%2Fstage/WindRiver-A4-RGB.jpg",
                     "https://www.youtube.com/watch?v=qnXzC7Kh-N0")
 
 lord_of_the_rings = media.Movie("Lord of the Rings", 
-                    "a marine on a different planet",
+                    "The fantasy world of J. R. R. Tolkien",
                     "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781608873821/the-lord-of-the-rings-9781608873821_hr.jpg",
                     "https://www.youtube.com/watch?v=V75dMMIW2B4")
 
 split = media.Movie("Split", 
-                    "a man with 43 different personalities",
+                    "A man with 23 different personalities",
                     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrUkpZ_VrCZkcSWpNL73eRCRFoGfKw0byUfSdJFI9pS1ZZtUgi",
                     "https://www.youtube.com/watch?v=1VqWDr2ldPI")
 
+# List of instances of the Movie Class created and given to the Function opem_movies_page
+# inside of the module fresh_tomatoes.py that creates the webpage 
 movies = [toy_story, avatar, shutter_island, wind_river, lord_of_the_rings, split]
-#fresh_tomatoes.open_movies_page(movies)
-#print(media.Movie.VALID_RATINGS)
-#print(media.Movie.__doc__)
+fresh_tomatoes.open_movies_page(movies)
+print("THE fresh_tomatoes.html HAS BEEN CREATED")
